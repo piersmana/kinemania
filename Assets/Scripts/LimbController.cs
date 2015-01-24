6e96aa6b1015a08d205fefe1dd06a3a39e544128
Martin Smith <martin@asimia.com>
2015-01-24 13:08:59 -0800

    next pass at monkey rig, using angular velocity and more constraints

--- Assets/Scripts/LimbController.cs
+++ Assets/Scripts/LimbController.cs
@@ -5,7 +5,7 @@ public class LimbController : MonoBehaviour {
 
 	public string horizontalInput;
 	public string verticalInput;
-	public float force = 10;
+	public float force = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -20,7 +20,20 @@ public class LimbController : MonoBehaviour {
 		float vertical = Input.GetAxis(verticalInput);
 
 		if (Mathf.Abs(horizontal) > 0 || Mathf.Abs(vertical) > 0) {
-			GetComponent<Rigidbody2D>().AddForce(new Vector2(horizontal, vertical) * force);
+			float angle = Mathf.Rad2Deg * ( Mathf.Atan2(-vertical, -horizontal) ) - 90;
+			//Debug.Log("input angle: " + angle);
+
+			float currentAngle = transform.rotation.eulerAngles.z;
+			//Debug.Log("current angle: " + currentAngle);
+
+			float maxDelta= force * Time.deltaTime;
+			float dAngle = Mathf.Clamp(dAngle, dAngle - maxDelta, dAngle + maxDelta);
+			//Debug.Log("dAngle: " + dAngle + " maxDelta: " + maxDelta);
+			rigidbody2D.AddTorque(dAngle);
+
+			// Clamp angular velocity as we approach the target vector to avoid overswing
+			if (dAngle < 5)
+				rigidbody2D.angularVelocity = dAngle;
 		}
 	}
 }
