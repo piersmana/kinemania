c21d8ca6a16da0c1fcee2045d6aa0e4afa7c01f3
z0lly <drzaal@gmail.com>
2015-01-23 22:35:58 -0800

    Starting to play with weighted objects / reaquainting myself with Unity. I hate you, Unity.
     Committer: z-lly <drzaal@gmail.com>
    
     On branch dev
     Changes to be committed:
    	new file:   ../Practicals.meta
    	new file:   ../Practicals/floor.prefab
    	new file:   ../Practicals/floor.prefab.meta
    	new file:   ../Scenes/zaal_doodads.unity
    	new file:   ../Scenes/zaal_doodads.unity.meta
    	new file:   activator.cs
    	new file:   activator.cs.meta
    	new file:   grabbable.cs
    	new file:   grabbable.cs.meta
    	new file:   testlifter.cs
    	new file:   testlifter.cs.meta

---
+++ Assets/Scripts/activator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class activator : MonoBehaviour {
+
+	private bool is_active;
+
+	// Use this for initialization
+	void Start () {
+		is_active = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void onOffToggle () {
+		is_active = !is_active;
+	}
+
+	void turnOn () {
+		is_active = true;
+	}
+
+	void turnOff () {
+		is_active = false;
+	}
+
+	bool isActive() {
+		return is_active;
+	}
+}
