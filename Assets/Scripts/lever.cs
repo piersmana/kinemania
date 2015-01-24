b5ec9ba1f413f2bc6291ec805163ac2c866dd974
z0lly <drzaal@gmail.com>
2015-01-24 13:20:22 -0800

    Got the lever working, it corresponds nicely to a working Door. Will be adding the zipline too.
     Changes to be committed:
    	modified:   Scenes/zaal_doodads.unity
    	modified:   Scripts/lever.cs
    	new file:   Scripts/leverBase.cs
    	new file:   Scripts/leverBase.cs.meta
    	new file:   Scripts/powerSource.cs
    	new file:   Scripts/powerSource.cs.meta
    	new file:   Scripts/powered.cs
    	new file:   Scripts/powered.cs.meta
    	new file:   Scripts/poweredDoor.cs
    	new file:   Scripts/poweredDoor.cs.meta
    	new file:   Sprites/lever_base.png
    	new file:   Sprites/lever_base.png.meta
    	new file:   Sprites/lever_leverl.png
    	new file:   Sprites/lever_leverl.png.meta
    	new file:   Sprites/tossables.png
    	new file:   Sprites/tossables.png.meta
    	modified:   ../ProjectSettings/TagManager.asset

--- Assets/Scripts/lever.cs
+++ Assets/Scripts/lever.cs
@@ -1,15 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class lever : MonoBehaviour {
+public class lever : powerSource {
+
+	public GameObject lever_base;
 
 	// Use this for initialization
 	void Start () {
-	
+		if ( is_on ) {
+			lever_base.GetComponent<SpriteRenderer>().sprite = lever_base.GetComponent<leverBase>().onState;
+		}
+		else {
+			lever_base.GetComponent<SpriteRenderer>().sprite = lever_base.GetComponent<leverBase>().offState;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if ( gameObject.GetComponent<HingeJoint2D>().jointAngle < -80 && !is_on) {
+			is_on = true;
+			lever_base.GetComponent<SpriteRenderer>().sprite = lever_base.GetComponent<leverBase>().onState;
+		}
+		if ( gameObject.GetComponent<HingeJoint2D>().jointAngle > -80 && is_on) {
+			is_on = false;
+			lever_base.GetComponent<SpriteRenderer>().sprite = lever_base.GetComponent<leverBase>().offState;
+		}
 	}
 }
---
+++ Assets/Scripts/powerSource.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class powerSource : MonoBehaviour {
+	
+	public bool is_on;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
