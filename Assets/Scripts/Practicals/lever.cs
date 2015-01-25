bcf7f91126b583658e6a93c090a14a000d0d5f27
z0lly <drzaal@gmail.com>
2015-01-24 23:37:02 -0800

    Moved a bunch of the scripts and things around to reorganize a little.

---
+++ Assets/Scripts/Practicals/lever.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lever : powerSource {
+
+	public GameObject lever_base;
+	public int id;
+	static public int max_id;
+
+	// Use this for initialization
+	void Start () {
+		if ( is_on ) {
+			lever_base.GetComponent<SpriteRenderer>().sprite = lever_base.GetComponent<leverBase>().onState;
+		}
+		else {
+			lever_base.GetComponent<SpriteRenderer>().sprite = lever_base.GetComponent<leverBase>().offState;
+		}
+		if (id > max_id) {
+			max_id = id;
+		}
+		if (id <= 0) {
+			id = max_id + 1;
+			max_id += 1;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( gameObject.GetComponent<HingeJoint2D>().jointAngle < -80 && !is_on) {
+			is_on = true;
+			lever_base.GetComponent<SpriteRenderer>().sprite = lever_base.GetComponent<leverBase>().onState;
+		}
+		if ( gameObject.GetComponent<HingeJoint2D>().jointAngle > -80 && is_on) {
+			is_on = false;
+			lever_base.GetComponent<SpriteRenderer>().sprite = lever_base.GetComponent<leverBase>().offState;
+		}
+	}
+}
---
+++ Assets/Scripts/Practicals/winPlatform.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class winPlatform : MonoBehaviour {
+
+	public Transform win_shower_whiskey;
+	public Transform win_shower_banana;
+	public bool has_won;
+
+	// Use this for initialization
+	void Start () {
+		has_won = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay2D( Collider2D other ) {
+		if ( !has_won && other.gameObject.name == "Body" ) {
+			if ( other.rigidbody2D.velocity.magnitude <= 0.3 ) {
+				Instantiate( win_shower_whiskey, new Vector3( 0, 12, -0.003F ),  Quaternion.identity );
+				Instantiate( win_shower_banana, new Vector3( 0, 12, -0.003F ),  Quaternion.identity );
+				has_won = true;
+			}
+		}
+	}
+}
