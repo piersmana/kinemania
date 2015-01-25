e63857714e9cd8452d263a9de53426c5103b5b75
z0lly <drzaal@gmail.com>
2015-01-25 13:00:34 -0800

    Gotta get started on this level, man. Starting to lay out the level. We are adding a little more music. Trying to work on that prefab for the monkey on the side, to get those limbs moving a little less... rancorously.  Changes to be committed: 	new file:   Assets/Audio/Music/The_Freak_Fandango_Orchestra_-_01_-_Monkey_Said.mp3 	new file:   Assets/Audio/Music/The_Freak_Fandango_Orchestra_-_01_-_Monkey_Said.mp3.meta 	modified:   Assets/Prefabs/MonkeyPrefab.prefab 	new file:   Assets/Prefabs/MonkeyPrefabFixing.prefab 	new file:   Assets/Prefabs/MonkeyPrefabFixing.prefab.meta 	new file:   Assets/Scenes/cold_open.unity 	new file:   Assets/Scenes/cold_open.unity.meta 	new file:   Assets/Scenes/level_1.unity 	new file:   Assets/Scenes/level_1.unity.meta 	modified:   Assets/Scenes/zaal_doodads.unity 	new file:   Assets/Scripts/GameManager.cs 	new file:   Assets/Scripts/GameManager.cs.meta 	new file:   Assets/Scripts/GameMenu.cs 	new file:   Assets/Scripts/GameMenu.cs.meta 	new file:   Assets/Sprites/game_jam_credis.png 	new file:   Assets/Sprites/game_jam_credis.png.meta 	new file:   Assets/Sprites/game_jam_credits.png 	new file:   Assets/Sprites/game_jam_credits.png.meta 	new file:   Assets/Sprites/press_A.png 	new file:   Assets/Sprites/press_A.png.meta 	new file:   Assets/Sprites/treeline.png 	new file:   Assets/Sprites/treeline.png.meta 	modified:   ProjectSettings/EditorBuildSettings.asset 	modified:   ProjectSettings/InputManager.asset

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	public bool stage_complete;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		stage_complete = false;
+		player = GameObject.Find("MonkeyPrefab");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody2D.velocity = new Vector2( (player.transform.position.x - transform.position.x) / 4 , 0F);
+	}
+
+	void youWin() {
+		stage_complete = true;
+	}
+}
---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameMenu : MonoBehaviour {
+
+	public float loadup;
+	public Transform the_credits;
+	public Transform the_menu;
+	public List<Transform> menu_buttons;
+
+	// Use this for initialization
+	void Start () {
+		loadup = 0F;
+		the_menu.GetComponent<SpriteRenderer>().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (the_menu.GetComponent<SpriteRenderer>().enabled == true) {
+
+			if ( loadup > 0 ) {
+				loadup -= 0.005F;
+				the_menu.GetComponent<SpriteRenderer>().color = new Color( 1F, 1F, 1F, 1F - loadup );
+				the_credits.GetComponent<SpriteRenderer>().color = new Color( 1F, 1F, 1F, loadup );
+			}
+		}
+		else {
+			loadup += 0.015F;
+			the_credits.GetComponent<SpriteRenderer>().color = new Color( 1F, 1F, 1F, loadup );
+			if (loadup >= 1F) {
+				
+				the_menu.GetComponent<SpriteRenderer>().enabled = true;
+				StartCoroutine("PressAButton");
+				the_menu.GetComponent<SpriteRenderer>().color = new Color( 1F, 1F, 1F, 0 );
+			}
+		}
+	}
+
+	IEnumerator PressAButton() {
+
+		SpriteRenderer press_a_indicator = menu_buttons[0].GetComponent<SpriteRenderer>();
+		press_a_indicator.enabled = true;
+		while ( !Input.GetButton("Action Button")  && !Input.GetKey( "return" )) {
+			float fade_factor = Mathf.Cos( 0.3F * Time.time * Mathf.PI );
+			fade_factor = fade_factor * fade_factor;
+			press_a_indicator.color = new Color(1F, 1F, 1F, fade_factor);
+			yield return null;
+		}
+		Application.LoadLevel ("zaal_doodads");
+	}
+}
