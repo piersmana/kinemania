35d7654df04ba546ddf71f3cf3399ecd34a37874
Martin Smith <martin@asimia.com>
2015-01-24 23:30:55 -0800

    Tweaking and started a penalty for large inputs

--- Assets/Scripts/EndEffectorInput.cs
+++ Assets/Scripts/EndEffectorInput.cs
@@ -6,11 +6,9 @@ public class EndEffectorInput : MonoBehaviour {
 	public string horizontalInput;
 	public string verticalInput;
 	public float radius = 1;
+	public float cooldownTime = 3;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	private float cooldownRemaining = 0;
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,8 +17,19 @@ public class EndEffectorInput : MonoBehaviour {
 		float vertical = Input.GetAxis(verticalInput);
 
 		Vector3 offset = new Vector3(horizontal, vertical, 0);
+		float magnitude = offset.magnitude;
+
+		if (magnitude > .5f) {
+			// Incur a larger penalty for bigger flings
+			cooldownRemaining = cooldownTime * magnitude;
+		}
+		float cooldownPenalty = cooldownRemaining / cooldownTime;
 
 		// Update position from parent, ignoring rotation
-		transform.position = transform.parent.position + offset * radius;
+		transform.position = transform.parent.position + offset * radius * (1 - cooldownPenalty);
+
+		cooldownRemaining -= Time.deltaTime;
+		if (cooldownRemaining < 0)
+			cooldownRemaining = 0;
 	}
 }
