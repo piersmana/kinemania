5e8cfb51a0f501c2d3a4d931f0642bc148ebec55
Martin Smith <martin@asimia.com>
2015-01-24 22:31:29 -0800

    spring end effector based monkey prototype

---
+++ Assets/Scripts/EndEffectorInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndEffectorInput : MonoBehaviour {
+
+	public string horizontalInput;
+	public string verticalInput;
+	public float radius = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float horizontal = Input.GetAxis(horizontalInput);
+		float vertical = Input.GetAxis(verticalInput);
+
+		Vector3 offset = new Vector3(horizontal, vertical, 0);
+
+		// Update position from parent, ignoring rotation
+		transform.position = transform.parent.position + offset * radius;
+	}
+}
---
+++ Assets/Scripts/LockPosition.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockPosition : MonoBehaviour {
+
+	public Transform parent;
+
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.localPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		transform.position = parent.transform.position + offset;
+	}
+}
