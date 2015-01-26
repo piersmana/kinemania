fd8f66d3b9cd2bea5f6f9d2e243e431a97b0d63f
Martin Smith <martin@asimia.com>
2015-01-25 16:38:18 -0800

    start screen

---
+++ Assets/Scripts/RandomForces.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomForces : MonoBehaviour {
+
+	private float force = 100;
+
+	// Use this for initialization
+	IEnumerator Start () {
+	
+		while (true) {
+			rigidbody2D.AddForce(new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)) * force);
+			yield return new WaitForSeconds(Random.Range(1, 4));
+		}
+	}
+}
